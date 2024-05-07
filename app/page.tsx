fd0a58ac28a507d0ee4461736acdafdc968eded7
moose-hub <infamollagames@gmail.com>
2024-05-07 10:34:34 +0100

    update: jest setup complete and testing continuous deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
-export default async function Index() {
-  return <div>Hello</div>;
+import Link from 'next/link'
+ 
+export default function Home() {
+  return (
+    <div>
+      <h6>Home</h6>
+      <Link href="/about">About</Link>
+    </div>
+  )
 }
