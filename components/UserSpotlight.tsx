da887b2ab227b3537c72bb2f3c4d705eb50c129e
Mohamedamin <ma@amin2468@gmail.com>
2024-05-09 12:18:32 +0100

    created a profile page for the user

--- components/UserSpotlight.tsx
+++ components/UserSpotlight.tsx
@@ -1,13 +1,18 @@
 import { FaCircleUser } from "react-icons/fa6";
+import { usePathname } from "next/navigation";
+import { profile } from "console";
+import Link from "next/link";
 
 export default function UserSpotlight() {
+  const pathName = usePathname()
+  const href = "/profile"
   return (
     <div className="flex items-center">
       <FaCircleUser fill="black" className="mr-2 h-14 w-14" />
       <div className="flex flex-col">
-        <span className="text-1xl font-bold tracking-normal text-black/60">
-          User Name
-        </span>
+      <Link href={href}><span className="text-1xl font-bold tracking-normal text-black/60">
+           User Name 
+        </span></Link>
         <span className="text-sm font-normal tracking-normal text-black/60">
           Location, UK
         </span>
