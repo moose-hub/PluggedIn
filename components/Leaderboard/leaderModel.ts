a25e193496eafd7ca026c13b7e3f3705e9980738
ma3hl1 <85696480+ma3hl1@users.noreply.github.com>
2024-05-09 14:15:47 +0100

    reformatted

--- components/Leaderboard/leaderModel.ts
+++ components/Leaderboard/leaderModel.ts
@@ -1,23 +1,23 @@
 export interface Leader {
-    image: string;
-        name: string;
-        numberOfSwipes: number;
+  image: string;
+  name: string;
+  numberOfSwipes: number;
 }
 
 export const leaders: Leader[] = [
-    {
-        image: "/public/avatars/cat.svg",
-        name: "Joeben",
-        numberOfSwipes: 6924739
-    },
-    {
-        image: "/public/avatars/puppy.png",
-        name: "Kevin",
-        numberOfSwipes: 4654657
-    },
-    {
-        image: "/public/avatars/mouse.jpg",
-        name: "Bruce",
-        numberOfSwipes: 2354688
-    },
-]
+  {
+    image: "/public/avatars/cat.svg",
+    name: "Joeben",
+    numberOfSwipes: 6924739,
+  },
+  {
+    image: "/public/avatars/puppy.png",
+    name: "Kevin",
+    numberOfSwipes: 4654657,
+  },
+  {
+    image: "/public/avatars/mouse.jpg",
+    name: "Bruce",
+    numberOfSwipes: 2354688,
+  },
+];
