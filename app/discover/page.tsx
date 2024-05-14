f59d55f4c7859a21d4950a6d8fb288b87f37e1f2
moose-hub <infamollagames@gmail.com>
2024-05-14 16:17:19 +0100

    init: first display of data from database

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,8 +1,13 @@
+import SongList from "@/components/SongList";
+
 const Discover = () => {
   return (
-    <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl p-8 font-extrabold">
-      Discover
-    </h1>
+    <>
+      <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl p-8 font-extrabold">
+        Discover
+      </h1>
+      <SongList />
+    </>
   );
 };
 
