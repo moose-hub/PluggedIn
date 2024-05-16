d988031d88e6236a12b66eb66dd37ce9048c80ed
Rhys Postans <rmp71171@icloud.com>
2024-05-16 12:35:05 +0100

    leaderboard complete mock

--- components/Leaderboard/Leaderboard.tsx
+++ components/Leaderboard/Leaderboard.tsx
@@ -1,19 +1,19 @@
 import { Leader, leaders } from "./leaderModel";
-
 import LeadingArtist from "./LeadingArtist";
 
 export default function Leaderboard() {
   const sortedLeaders = [...leaders].sort(
     (a, b) => b.numberOfSwipes - a.numberOfSwipes,
   );
+
   return (
     <>
-      <div className="flex items-center ">
-        <div className="bg-[#e6e6e6] rounded ">
+      <div className="flex items-center mt-8 ">
+        <div className="rounded ">
           {sortedLeaders.map((leader: Leader) => (
             <LeadingArtist
               key={leader.key}
-              image={leader.image}
+              image={`https://i.pravatar.cc/300?img=${leader.key}`}
               name={leader.name}
               numberOfSwipes={leader.numberOfSwipes}
             />
