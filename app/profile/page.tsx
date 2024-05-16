056b776371bcac564ec39ae95516a14f3f018a05
Mohamedamin <ma@amin2468@gmail.com>
2024-05-16 10:26:42 +0100

    made changes so that content renders under the line when a button is clicked. Content will now also render one at a time.

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -15,6 +15,7 @@ interface UserProfileProps {
 }
 
 const Profile = () => {
+  const [activeButton, setActiveButton] = useState<number | null>(null);
   const [userData, setUserData] = useState<UserProfileProps | null>(null);
 
   useEffect(() => {
@@ -41,52 +42,139 @@ const Profile = () => {
     return <div>Loading...</div>;
   }
 
+  const content = () => {
+    switch (activeButton) {
+      case 1:
+        return <p>Library content goes here</p>;
+      case 2:
+        return <p>reviews go down here</p>;
+      case 3:
+        return <p>contributions go down here</p>;
+      default:
+        return <p> </p>;
+    }
+  };
+
   return (
-    <div className="container mx-auto p-4">
-      <div className="flex items-start gap-4">
-        {" "}
-        {/* Change to flex for inline layout */}
-        <img
-          src={userData.profileImage}
-          alt="Profile"
-          className="rounded-3xl w-64 h-64 object-cover mb-6 border-4 border-pi-purple-main" // Removed col-span-1
-        />
-        <div className="flex flex-col justify-start mt-5">
+    <>
+      <div className="container mx-auto p-4">
+        <div className="flex items-start gap-4">
           {" "}
-          <h1 className="text-6xl font-bold">
-            {userData.name}{" "}
-            <div
-              className="flex items-center py-1"
-              style={{ display: "inline" }}
-            >
-              <Followbtn />
-            </div>
-          </h1>
-          <p className="text-sm text-gray-600">@{userData.username}</p>
-          <p className="text-md mt-2">{userData.bio}</p>
-        </div>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <div className="flex flex col-3 mt-4 h-1 bg-gray-300 w-full">
-          <div>
-            <UploadedSongs />
-          </div>
-          <div>
-            <Review />
-          </div>
-          <div>
-            <ContributionForm />
+          {/* Change to flex for inline layout */}
+          <img
+            src={userData.profileImage}
+            alt="Profile"
+            className="rounded-3xl w-64 h-64 object-cover mb-6 border-4 border-pi-purple-main" // Removed col-span-1
+          />
+          <div className="flex flex-col justify-start mt-5">
+            {" "}
+            <h1 className="text-6xl font-bold">
+              {userData.name}{" "}
+              <div
+                className="flex items-center py-1"
+                style={{ display: "inline" }}
+              >
+                <Followbtn />
+              </div>
+            </h1>
+            <p className="text-sm text-gray-600">@{userData.username}</p>
+            <p className="text-md mt-2">{userData.bio}</p>
           </div>
         </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        ></div>
       </div>
-    </div>
+      <div className=" justify-center min-h-screen py-2">
+        <div className=" space-x-4">
+          <button
+            className="px-4 py-2 border-4"
+            onClick={() => {
+              setActiveButton(1);
+            }}
+          >
+            Library{" "}
+          </button>
+          <button
+            className="px-4 py-2 border-4"
+            onClick={() => {
+              setActiveButton(2);
+            }}
+          >
+            Reviews
+          </button>
+          <button
+            className="px-4 py-2 border-4"
+            onClick={() => {
+              setActiveButton(3);
+            }}
+          >
+            Contribute
+          </button>
+        </div>
+        <div className="border-b-2 border-gray-300 w-full my-4"></div>
+        <div className="content mt-4">{content()}</div>
+      </div>
+    </>
   );
 };
 
 export default Profile;
+
+//   return (
+//     <div className="container mx-auto p-4">
+//       <div className="flex items-start gap-4">
+//         {" "}
+//         {/* Change to flex for inline layout */}
+//         <img
+//           src={userData.profileImage}
+//           alt="Profile"
+//           className="rounded-3xl w-64 h-64 object-cover mb-6 border-4 border-pi-purple-main" // Removed col-span-1
+//         />
+//         <div className="flex flex-col justify-start mt-5">
+//           {" "}
+//           <h1 className="text-6xl font-bold">
+//             {userData.name}{" "}
+//             <div
+//               className="flex items-center py-1"
+//               style={{ display: "inline" }}
+//             >
+//               <Followbtn />
+//             </div>
+//           </h1>
+//           <p className="text-sm text-gray-600">@{userData.username}</p>
+//           <p className="text-md mt-2">{userData.bio}</p>
+//         </div>
+//       </div>
+//       <div
+//         style={{
+//           display: "flex",
+//           flexDirection: "row",
+//           justifyContent: "space-between",
+//         }}
+//       >
+
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Profile;
+
+// <div className="flex flex col-3 mt-4 h-1 bg-gray-300 w-full">
+//           <div>
+//             <UploadedSongs />
+//           </div>
+//           <div>
+//             <Review />
+//           </div>
+//           <div>
+//             <ContributionForm />
+//           </div>
+//         </div>
+
+// const Profile = () => {
