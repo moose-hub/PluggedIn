9beb967f54a135918a4f7fabc80a7d7c01bf07f4
moose-hub <infamollagames@gmail.com>
2024-05-21 16:11:37 +0100

    update: login working with new conditional rendering of user spotlight

--- components/UserSpotlight.tsx
+++ components/UserSpotlight.tsx
@@ -1,10 +1,31 @@
 import Link from "next/link";
 import Image from "next/image";
-import { FaEllipsisVertical } from "react-icons/fa6";
-import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import DropDown from "./DropDown";
+import { useAuth } from "@/hooks/useAuth";
 
 export default function UserSpotlight() {
   const href = "/profile";
+  const { user, isLoading, error, signOut } = useAuth();
+
+  const handleEditProfile = () => {};
+
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
+
+  const menuItems = [
+    { label: "Edit Profile", onClick: handleEditProfile },
+    { label: "Sign Out", onClick: handleSignOut },
+  ];
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="flex items-center w-full">
       <div className="flex items-center w-80">
@@ -28,13 +49,9 @@ export default function UserSpotlight() {
       </div>
       <div
         className="
-        flex items-center 
-        text-xl 
-        p-2 rounded-full
-        hover:cursor-pointer 
-        hover:bg-pi-offwhite-shadow"
+        "
       >
-        <FaEllipsisVertical />
+        <DropDown menuItems={menuItems} />
       </div>
     </div>
   );
